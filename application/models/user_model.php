2ae5aec3286841011d944819d62524f6608a0edb
Chintan Shah <chintan@wohlig.com>
2015-05-20 15:34:31 +0530

    git change

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -6,10 +6,10 @@ class User_model extends CI_Model
 	protected $id,$username ,$password;
 	public function validate($username,$password )
 	{
-		
+
 		$password=md5($password);
 		$query ="SELECT `user`.`id`,`user`.`name` as `name`,`email`,`user`.`accesslevel`,`accesslevel`.`name` as `access` FROM `user`
-		INNER JOIN `accesslevel` ON `user`.`accesslevel` = `accesslevel`.`id` 
+		INNER JOIN `accesslevel` ON `user`.`accesslevel` = `accesslevel`.`id`
 		WHERE `email` LIKE '$username' AND `password` LIKE '$password' AND `status`=1 AND `accesslevel` IN (1,2) ";
 		$row =$this->db->query( $query );
 		if ( $row->num_rows() > 0 ) {
@@ -30,8 +30,8 @@ class User_model extends CI_Model
 		else
 			return false;
 	}
-	
-	
+
+
 	public function create($name,$email,$password,$accesslevel,$status,$socialid,$logintype,$image,$json,$shopname,$membershipno,$address,$description,$website,$shopcontact1,$shopcontact2,$shopemail,$purchasebalance,$salesbalance,$area,$shoplogo,$percentpayment)
 	{
 		$data  = array(
@@ -57,11 +57,11 @@ class User_model extends CI_Model
 			'salesbalance' => $salesbalance,
 			'shoplogo' => $shoplogo,
 			'percentpayment' => $percentpayment
-						
+
 		);
 		$query=$this->db->insert( 'user', $data );
 		$id=$this->db->insert_id();
-        
+
 		if(!$query)
 			return  0;
 		else
@@ -85,9 +85,9 @@ class User_model extends CI_Model
 
         $password = "";
         for($i=0; $i<$length; $i++) {
-            $current_letter = $use_upper_case ? (rand(0,1) ? strtoupper($selection[(rand() % strlen($selection))]) : $selection[(rand() % strlen($selection))]) : $selection[(rand() % strlen($selection))];            
+            $current_letter = $use_upper_case ? (rand(0,1) ? strtoupper($selection[(rand() % strlen($selection))]) : $selection[(rand() % strlen($selection))]) : $selection[(rand() % strlen($selection))];
             $password .=  $current_letter;
-        }                
+        }
 
       return $password;
     }
@@ -106,10 +106,10 @@ class User_model extends CI_Model
 		{
 			$query .= " WHERE `user`.`accesslevel`> '$accesslevel' ";
 		}
-		
+
 	   $query.=" ORDER BY `user`.`id` ASC LIMIT $startfrom,$totallength";
 		$query=$this->db->query($query)->result();
-        
+
         $return=new stdClass();
         $return->query=$query;
         $return->totalcount=$this->db->query("SELECT count(*) as `totalcount` FROM `user`
@@ -123,7 +123,7 @@ class User_model extends CI_Model
 		$query=$this->db->get( 'user' )->row();
 		return $query;
 	}
-	
+
 	public function edit($id,$name,$email,$password,$accesslevel,$status,$socialid,$logintype,$image,$json,$shopname,$membershipno,$address,$description,$website,$shopcontact1,$shopcontact2,$shopemail,$purchasebalance,$salesbalance,$area,$shoplogo,$percentpayment)
 	{
 		$data  = array(
@@ -153,10 +153,10 @@ class User_model extends CI_Model
 			$data['password'] =md5($password);
 		$this->db->where( 'id', $id );
 		$query=$this->db->update( 'user', $data );
-        
+
 		return 1;
 	}
-    
+
 	public function getuserimagebyid($id)
 	{
 		$query=$this->db->query("SELECT `image` FROM `user` WHERE `id`='$id'")->row();
@@ -178,7 +178,7 @@ class User_model extends CI_Model
 		else
 			return  1;
 	}
-    
+
     public function getuserdropdown()
 	{
 		$query=$this->db->query("SELECT * FROM `user`  ORDER BY `id` ASC")->result();
@@ -189,10 +189,10 @@ class User_model extends CI_Model
 		{
 			$return[$row->id]=$row->name;
 		}
-		
+
 		return $return;
 	}
-    
+
 	public function getaccesslevels()
 	{
 		$return=array();
@@ -226,7 +226,7 @@ class User_model extends CI_Model
 					}
 				}
 			}
-	
+
 		return $return;
 	}
     public function getstatusdropdown()
@@ -238,21 +238,21 @@ class User_model extends CI_Model
 		{
 			$return[$row->id]=$row->name;
 		}
-		
+
 		return $return;
 	}
 	public function sendemail($email,$membershipno,$password){
 	$this->load->library('email');
 $this->email->from('pthakare33@gmail.com', 'pooja');
-$this->email->to($email); 
+$this->email->to($email);
 $this->email->subject('Auto generated Password');
-$this->email->message('Your Membershipid no is'."$membershipno".'and password is'."$password");	
+$this->email->message('Your Membershipid no is'."$membershipno".'and password is'."$password");
 
 $this->email->send();
 
 echo $this->email->print_debugger();
 	}
-    
+
 	function changestatus($id)
 	{
 		$query=$this->db->query("SELECT `status` FROM `user` WHERE `id`='$id'")->row();
@@ -282,7 +282,7 @@ echo $this->email->print_debugger();
 			'city' => $city,
 			'pincode' => $pincode,
 		);
-		
+
 		$this->db->where( 'id', $id );
 		$query=$this->db->update( 'user', $data );
 		if($query)
@@ -291,7 +291,7 @@ echo $this->email->print_debugger();
 		}
 		return 1;
 	}
-	
+
 	function saveuserlog($id,$status)
 	{
 //		$fromuser = $this->session->userdata('id');
@@ -302,9 +302,9 @@ echo $this->email->print_debugger();
 		$query2=$this->db->insert( 'userlog', $data2 );
         $query=$this->db->query("UPDATE `user` SET `status`='$status' WHERE `id`='$user'");
 	}
-    function signup($email,$password) 
+    function signup($email,$password)
     {
-         $password=md5($password);   
+         $password=md5($password);
         $query=$this->db->query("SELECT `id` FROM `user` WHERE `email`='$email' ");
         if($query->num_rows == 0)
         {
@@ -318,18 +318,18 @@ echo $this->email->print_debugger();
             );
 
             $this->session->set_userdata($newdata);
-            
+
           //  $queryorganizer=$this->db->query("INSERT INTO `organizer`(`name`, `description`, `email`, `info`, `website`, `contact`, `user`) VALUES(NULL,NULL,NULL,NULL,NULL,NULL,'$user')");
-            
-            
+
+
            return $user;
         }
         else
          return false;
-        
-        
+
+
     }
-    function login($membershipno,$password) 
+    function login($membershipno,$password,$token)
     {
         $password=md5($password);
         $query=$this->db->query("SELECT `id` FROM `user` WHERE `membershipno`='$membershipno' AND `password`= '$password'");
@@ -337,7 +337,9 @@ echo $this->email->print_debugger();
         {
             $user=$query->row();
             $user=$user->id;
-            
+
+						$query=$this->db->query("UPDATE `user` SET `token`='$token' WHERE `id`='$id'");
+
 
             $newdata = array(
                 'membershipno'     => $membershipno,
@@ -355,7 +357,7 @@ echo $this->email->print_debugger();
 
 
     }
-	
+
     function authenticate() {
         $is_logged_in = $this->session->userdata( 'logged_in' );
         //print_r($is_logged_in);
@@ -367,8 +369,8 @@ echo $this->email->print_debugger();
          return $userid;
         }
     }
-    
-    function frontendauthenticate($email,$password) 
+
+    function frontendauthenticate($email,$password)
     {
         $query=$this->db->query("SELECT `id`, `name`, `email`, `accesslevel`, `timestamp`, `status`, `image`, `username`, `socialid`, `logintype`, `json` FROM `user` WHERE `email` LIKE '$email' AND `password`='$password' LIMIT 0,1");
         if ($query->num_rows() > 0)
@@ -395,9 +397,9 @@ echo $this->email->print_debugger();
                     $this->saveuserlog($id,$status);
 //                }
             }
-            
+
         $query2=$this->db->query("SELECT `id`, `name`, `email`, `accesslevel`, `timestamp`, `status`, `image`, `username`, `socialid`, `logintype`, `json` FROM `user` WHERE `id`='$id' LIMIT 0,1")->row();
-            
+
         $newdata        = array(
 				'id' => $query2->id,
 				'email' => $query2->email,
@@ -407,8 +409,8 @@ echo $this->email->print_debugger();
 				'logged_in' => 'true',
 			);
 			$this->session->set_userdata( $newdata );
-            
-            
+
+
             $accesslevel=$query->accesslevel;
             if($accesslevel==2)
             {
@@ -416,13 +418,13 @@ echo $this->email->print_debugger();
             }
         	return $data;
         }
-        else 
+        else
         {
         	return false;
         }
     }
-    
-    function frontendregister($name,$email,$password,$socialid,$logintype,$json) 
+
+    function frontendregister($name,$email,$password,$socialid,$logintype,$json)
     {
         $data  = array(
 			'name' => $name,
@@ -437,7 +439,7 @@ echo $this->email->print_debugger();
 		$query=$this->db->insert( 'user', $data );
 		$id=$this->db->insert_id();
         $queryselect=$this->db->query("SELECT * FROM `user` WHERE `id` LIKE '$id' LIMIT 0,1")->row();
-        
+
         $accesslevel=$queryselect->accesslevel;
 //        $queryselect=$query;
         $data1['user']=$queryselect;
@@ -447,18 +449,18 @@ echo $this->email->print_debugger();
         }
         return $data1;
     }
-    
+
 	function getallinfoofuser($id)
 	{
 		$user = $this->session->userdata('accesslevel');
 		$query="SELECT DISTINCT `user`.`id` as `id`,`user`.`firstname` as `firstname`,`user`.`lastname` as `lastname`,`accesslevel`.`name` as `accesslevel`	,`user`.`email` as `email`,`user`.`contact` as `contact`,`user`.`status` as `status`,`user`.`accesslevel` as `access`
 		FROM `user`
-	   INNER JOIN `accesslevel` ON `user`.`accesslevel`=`accesslevel`.`id` 
+	   INNER JOIN `accesslevel` ON `user`.`accesslevel`=`accesslevel`.`id`
        WHERE `user`.`id`='$id'";
 		$query=$this->db->query($query)->row();
 		return $query;
 	}
-    
+
 	public function getlogintypedropdown()
 	{
 		$query=$this->db->query("SELECT * FROM `logintype`  ORDER BY `id` ASC")->result();
@@ -468,10 +470,10 @@ echo $this->email->print_debugger();
 		{
 			$return[$row->id]=$row->name;
 		}
-		
+
 		return $return;
 	}
-    
+
 	public function frontendlogout($user)
 	{
         $query=$this->db->query("SELECT `id`, `name`, `email`, `accesslevel`, `timestamp`, `status`, `image`, `username`, `socialid`, `logintype`, `json` FROM `user` WHERE `id`='$user' LIMIT 0,1")->row();
@@ -495,17 +497,17 @@ echo $this->email->print_debugger();
 //            }
         }
 //        $updatequery=$this->db->query("UPDATE `user` SET `status`=5 WHERE `id`='$user'");
-        
+
 //        if(!$updatequery)
 //            return 0;
 //        else
 //        {
-            
+
 		$this->session->sess_destroy();
             return 1;
 //        }
 	}
-	
+
     function sociallogin($user_profile,$provider)
     {
         $query=$this->db->query("SELECT * FROM `user` WHERE `user`.`socialid`='$user_profile->identifier'");
