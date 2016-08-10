952a6d84b1cfc2f245d45b02d1a2a69e45139ec8
Pooja <pthakare33@gmail.com>
2016-08-10 16:21:58 +0530

    gterdesg

--- application/views/backend/viewhotel.php
+++ application/views/backend/viewhotel.php
@@ -26,7 +26,7 @@
 		</section>
 		<script>
 			function drawtable(resultrow) {
-				return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.hotelname + "</td><td>" + resultrow.timestamp + "</td><td><a class='btn btn-primary btn-xs' href='<?php echo site_url('site/edithotel?id=');?>" + resultrow.id + "'><i class='icon-pencil'></i></a><a class='btn btn-danger btn-xs' onclick=\"return confirm('Are you sure you want to delete?');\" href='<?php echo site_url('site/deletehotel?id='); ?>"+resultrow.id+"'><i class='icon-trash '></i></a><a class='btn btn-primary btn-xs' href='<?php echo site_url('site/sendmessagetoall?id=');?>" + resultrow.id + "'><i class='icon-share-sign'></i></a></td></tr>";
+				return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.hotelname + "</td><td>" + resultrow.timestamp + "</td><td><a class='btn btn-primary btn-xs' href='<?php echo site_url('site/edithotel?id=');?>" + resultrow.id + "'><i class='icon-pencil'></i></a><a class='btn btn-danger btn-xs' onclick=\"return confirm('Are you sure you want to delete?');\" href='<?php echo site_url('site/deletehotel?id='); ?>"+resultrow.id+"'><i class='icon-trash '></i></a></td></tr>";
 			}
 			generatejquery("<?php echo $base_url;?>");
 		</script>
